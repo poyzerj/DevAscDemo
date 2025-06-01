66abb0b8caece10f045634a8245d8ff2676e28f2
Joe Poyzer <poyzerj@live.com>
2025-06-01 00:45:44 -0500

    Added welcome greeting

--- Feature/myfeature.py
+++ Feature/myfeature.py
@@ -8,3 +8,5 @@ def greet(name):
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
     print(greet(user_name))
+    print("Welcome to DevAsc")
+# This is a new feature script that greets the user.
